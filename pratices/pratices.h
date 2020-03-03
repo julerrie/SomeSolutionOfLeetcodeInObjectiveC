b6ad1d2b5a5bf4c33a8ae3a32a614657697060f5
Lan Luo <r-ran@tg-inet.co.jp>
2020-03-03 12:45:50 +0900

    Initial Commit

---
+++ pratices/pratices.h
@@ -0,0 +1,19 @@
+//
+//  pratices.h
+//  pratices
+//
+//  Created by Lan Luo on 2020/03/03.
+//  Copyright © 2020 Lan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for pratices.
+FOUNDATION_EXPORT double praticesVersionNumber;
+
+//! Project version string for pratices.
+FOUNDATION_EXPORT const unsigned char praticesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <pratices/PublicHeader.h>
+
+
